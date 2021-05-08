f03c3dabc14df2fca4f333eff4c876f3e11595cc
Udara Vimukthi <uvlakshan@gmail.com>
2021-05-08 23:00:04 +0530

    Hello world with MPI programming

---
+++ MPI programing/MPI programing.cpp
@@ -0,0 +1,48 @@
+// MPI programing.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+//#include <iostream>
+
+//int main()
+//{
+   // std::cout << "Hello World!\n";
+//}
+
+#include <mpi.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+	// Initialize the MPI environment
+	MPI_Init(NULL, NULL);
+
+	// Get the number of processes
+	int world_size;
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+	// Get the rank of the process
+	int world_rank;
+	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+	// Get the name of the processor
+	char processor_name[MPI_MAX_PROCESSOR_NAME];
+	int name_len;
+	MPI_Get_processor_name(processor_name, &name_len);
+
+	// Print off a hello world message
+	printf("Hello world from processor %s, rank %d out of %d processors\n",
+		processor_name, world_rank, world_size);
+
+	// Finalize the MPI environment.
+	MPI_Finalize();
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
