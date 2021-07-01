62cede0d48949b1b3c29fe707698642f32306365
Udara Vimukthi <uvlakshan@gmail.com>
2021-07-01 23:13:42 +0530

    Added LB4 example 6

---
+++ LB4_Example6/174083C_LB4_Example6/174083C_LB4_Example6.cpp
@@ -0,0 +1,121 @@
+// 174083C_LB4_Example6.cpp 
+
+
+#include <stdio.h>
+#include <mpi.h>
+#include <stdlib.h>
+#define MAX_SIZE 10000
+void Merge(int* a, int* b, int start, int middle, int end)
+{
+	int na1, na2, nb, i;
+	na1 = start;
+	nb = start;
+	na2 = middle + 1;
+	while ((na1 <= middle) && (na2 <= end)) {
+		if (a[na1] <= a[na2]) {
+			b[nb++] = a[na1++];
+		}
+		else {
+			b[nb++] = a[na2++];
+		}
+	}
+	if (na1 <= middle) {
+		for (i = na1; i <= middle; i++) {
+			b[nb++] = a[i];
+		}
+	}
+	if (na2 <= end) {
+		for (i = na2; i <= end; i++) {
+			b[nb++] = a[i];
+		}
+	}
+	for (i = start; i <= end; i++) {
+		a[i] = b[i];
+	}
+}
+void Merge_Sort(int* a, int* b, int start, int end)
+{
+	int middle;
+
+	if (start < end) {
+		middle = (start + end) / 2;
+		Merge_Sort(a, b, start, middle);
+		Merge_Sort(a, b, middle + 1, end);
+		Merge(a, b, start, middle, end);
+	}
+}
+int main(int argc, char* argv[])
+{
+	int arr[MAX_SIZE];
+	int comm_sz;
+	int my_rank;
+	int length = 0;
+	int size;
+	int* sub;
+	int* tmp;
+	int* result = NULL;
+	int* tmp_for_last;
+	int i;
+	int count = 20;
+
+	//FILE* fpread;
+	// read file
+	for (i = 0; i < MAX_SIZE; i++) {
+		arr[i] = -9999;
+	}
+	/*fpread = fopen(argv[1], "r");
+	if (fpread == NULL) {
+		printf("Read error!");
+		return 0;
+	}
+	for (i = 0; !feof(fpread); i++) {
+		fscanf(fpread, "%d ", &arr[i]);
+		length++;
+	}*/
+
+	/* Assertation check for wether argumnent is given or
+	not as a command line argument*/
+	if (argc > 1) {
+		count = atoi(argv[1]);
+	}
+	for (int i = 0; i < count; i++) {
+		arr[i] = rand() % 10000;
+		length++;
+	}
+
+	MPI_Init(NULL, NULL);
+	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+	// Calculate the size of the array required by each process
+	size = length / comm_sz;
+	// Open up space for the array used by the process
+	sub = (int*)malloc(size * sizeof(int));
+	// Distribute the array elements to each process
+	MPI_Scatter(arr, size, MPI_INT, sub, size, MPI_INT, 0, MPI_COMM_WORLD);
+	// Temporary array needed for merge sort
+	tmp = (int*)malloc(size * sizeof(int));
+	Merge_Sort(sub, tmp, 0, size - 1);
+	if (my_rank == 0) {
+		// Open up the array space required for the result(as large as the input array)
+		result = (int*)malloc(length * sizeof(int));
+	}
+	// collect the array sorted by each process to process 0
+	MPI_Gather(sub, size, MPI_INT, result, size, MPI_INT, 0, MPI_COMM_WORLD);
+	if (my_rank == 0) {
+		tmp_for_last = (int*)malloc(length * sizeof(int));
+		Merge_Sort(result, tmp_for_last, 0, length - 1);
+		// print the result
+		printf("\nThe result is: \n");
+		for (i = 0; i < length; i++) {
+			printf("%d ", result[i]);
+		}
+		printf("\n");
+		free(result);
+		free(tmp_for_last);
+	}
+	free(sub);
+	free(tmp);
+
+	MPI_Finalize();
+	return 0;
+}
